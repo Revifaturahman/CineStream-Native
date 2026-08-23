c9ca078ffe129824380603b8fb822728a18c7261
Revifaturahman <123242308+Revifaturahman@users.noreply.github.com>
2026-08-23 19:44:19 +0700

    Add Google Search Console verification

--- includes/header.php
+++ includes/header.php
@@ -11,6 +11,7 @@ $currentSearch = isset($_GET['search']) ? trim($_GET['search']) : null;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="uhoLPZigkmGubHEpLD4OGRBcoPWJ06eIolpIyhJjwxk" />
     <?php
         $pageTitle = APP_NAME . ' - ' . APP_TAGLINE;
 
